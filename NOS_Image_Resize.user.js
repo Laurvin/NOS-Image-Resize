a2c52d321ed98ecc662b66b23a0d87a47ebafd61
Laurvin <github@dbbv.com>
2020-05-28 10:20:15 +0200

    Update NOS_Image_Resize.user.js

--- NOS_Image_Resize.user.js
+++ NOS_Image_Resize.user.js
@@ -3,7 +3,7 @@
 // @namespace NOS Image Resize
 // @author Laurvin
 // @description When directly viewing an image on NOS.nl, ensures that the highest resolution image is loaded. (Direct rewrite of Tumblr Image Size)
-// @version 1.3
+// @version 1.4
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/NOS-Image-Resize/raw/master/NOS_Image_Resize.user.js
 // @include https://nos.nl/data/image/*
@@ -12,19 +12,21 @@
 // @run-at document-idle
 // ==/UserScript==
 
-var sizes = [ 'original', '2048x1152', '1200x675', '1008x567', '864x486' ];
+var sizes = [ 'original', '2048x1152', '1536x864a', '1200x675', '1008x567', '864x486' ];
 
 function checkSize(index) {
     if (index >= sizes.length) return;
-    var url = window.location.href.replace(/[0-9]{3,4}[x][0-9]{3,4}|xxl/, sizes[index]);
+    var url = window.location.href.replace(/[0-9]{3,4}[x][0-9]{3,4}[a]{0,1}|xxl/, sizes[index]);
     if (url == window.location.href) return;
     $.ajax({
         url: url,
         type: 'HEAD',
         success: function(data, textStatus, jqXHR) {
+            console.log('success');
             window.location.replace(url);
         },
         error: function(jqXHR, textStatus, errorThrown) {
+            console.log('error');
             checkSize(index + 1);
         }
     });
